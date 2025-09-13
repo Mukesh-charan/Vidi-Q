901be25be4b10f8c0904b7530792f87844e46222
Rohit Mugalya <rohitmugalya@gmail.com>
2025-09-13 21:03:02 +0530

    excluding temp/

--- output/generated_manim_script.py
+++ output/generated_manim_script.py
@@ -1,63 +1,54 @@
 from manim import *
 
-class BinarySearch(Scene):
+class LinearRegression(Scene):
     def construct(self):
-        # Title
-        title = Text("Binary Search").scale(1.5)
-        self.play(Write(title))
-        self.wait(1)
-        self.play(FadeOut(title))
-
-        # Sorted Array
-        array = [2, 5, 7, 8, 11, 12]
-        array_mob = VGroup(*[Text(str(x)).scale(0.7) for x in array])
-        array_mob.arrange(RIGHT, buff=0.5)
-        array_mob.set_color(YELLOW)
-        self.play(Write(array_mob))
+        # Data points
+        points = [
+            (1, 2), (2, 3), (3, 5), (4, 4), (5, 6), (6, 7)
+        ]
+        dots = VGroup(*[Dot(np.array([x, y, 0])) for x, y in points])
+        self.play(Create(dots))
         self.wait(1)
 
-        # Target Value
-        target = 11
-        target_text = Text(f"Target: {target}").to_edge(UP)
-        self.play(Write(target_text))
+        # Scatter plot label
+        scatter_label = Tex("Scatter Plot").to_edge(UP)
+        self.play(Write(scatter_label))
         self.wait(1)
 
-        # Search Process
-        low = 0
-        high = len(array) - 1
-        rect = SurroundingRectangle(array_mob[low:high+1], color=BLUE)
-        self.play(Create(rect))
-
-        while low <= high:
-            mid = (low + high) // 2
-            mid_text = Text(f"Mid: {array[mid]}").next_to(array_mob[mid], DOWN)
-            self.play(Write(mid_text))
-            self.wait(1)
-
-            if array[mid] == target:
-                self.play(array_mob[mid].animate.set_color(GREEN))
-                self.wait(2)
-                break
-
-            elif array[mid] < target:
-                low = mid + 1
-                rect = SurroundingRectangle(array_mob[low:high+1], color=BLUE)
-                self.play(Transform(rect, rect))
-                self.wait(1)
-                self.play(FadeOut(mid_text))
-                
-            else:
-                high = mid - 1
-                rect = SurroundingRectangle(array_mob[low:high+1], color=BLUE)
-                self.play(Transform(rect, rect))
-                self.wait(1)
-                self.play(FadeOut(mid_text))
-
-        self.play(FadeOut(rect), FadeOut(target_text))
+        # Best fit line
+        x_coords = [x for x, y in points]
+        y_coords = [y for x, y in points]
+        A = np.vstack([x_coords, np.ones(len(x_coords))]).T
+        m, c = np.linalg.lstsq(A, y_coords, rcond=None)[0]
+        line_func = lambda x: m * x + c
+        line = FunctionGraph(line_func, x_range=[0.5, 6.5], color=YELLOW)
+        self.play(Create(line))
         self.wait(1)
 
-        # Conclusion
-        conclusion = Text("Binary Search Found!").scale(1.2).set_color(GREEN)
-        self.play(Write(conclusion))
+        # Line equation
+        equation = MathTex("y = mx + c").next_to(line, UP)
+        self.play(Write(equation))
+        self.wait(1)
+
+        # m and c values
+        m_val = MathTex("m =", str(round(m, 2))).next_to(equation, DOWN)
+        c_val = MathTex("c =", str(round(c, 2))).next_to(m_val, RIGHT)
+
+        self.play(Write(m_val), Write(c_val))
         self.wait(2)
-        self.play(FadeOut(conclusion), FadeOut(array_mob))
+
+
+        # Explanation
+        explanation = Tex("This line minimizes the distance to all points").next_to(equation, DOWN, buff=1)
+        self.play(Write(explanation))
+        self.wait(2)
+
+        #Prediction
+        x_new = 7
+        y_new = line_func(x_new)
+        new_point = Dot(np.array([x_new,y_new,0]), color=RED)
+        prediction_text = Tex(f"Prediction for x={x_new}: y={y_new:.2f}").next_to(explanation, DOWN)
+        self.play(Create(new_point),Write(prediction_text))
+        self.wait(3)
+
+        self.play(*[FadeOut(mob) for mob in self.mobjects])
